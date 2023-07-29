5fc6b3474a81a5313497a28ed6810ecd916542fb
Rishvic Pushpakaran <rishvic@gmail.com>
2023-07-30 01:30:11 +0530

    Added graphviz-react

--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,9 +1,15 @@
 'use client';
+import {Graphviz} from 'graphviz-react';
+import {useState} from 'react';
 
 export default function Home() {
+
+  const [dotStr, setDotStr] = useState<string>("");
+
   const submitHandler = async () => {
     const rregex = (await import('rregex'));
-    const post = rregex.get_debug_graph_json("a|b*");
+    const post = rregex.get_debug_graph_json("a|bc*");
+    setDotStr(post.get_dot_str());
     console.log(post.get_dot_str());
     console.log(post.get_start());
     console.log(post.get_fin());
@@ -14,17 +20,20 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">RRegex</h1>
-      <button
-        onClick={() => {
-          (async () => {
-            await submitHandler();
-          })().catch((e: unknown) => {
-            console.error(`Could not submit: ${e}`);
-          });
-        }}
-      >
-        Click here
-      </button>
+      <p>
+        <button
+          onClick={() => {
+            (async () => {
+              await submitHandler();
+            })().catch((e: unknown) => {
+              console.error(`Could not submit: ${e}`);
+            });
+          }}
+        >
+          Click here
+        </button>
+      </p>
+      {dotStr && <Graphviz dot={dotStr} />}
     </main>
   );
 }
